8e324b16fc8c65e0e0f81089f13d77b0323cae7f
Harsh Srivastava <85394976+harsh373@users.noreply.github.com>
2025-12-31 20:30:51 +0530

    Format vite.config.ts for consistency

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
   server: {
-    port: 5173, // ✅ match backend CORS
+    port: 5173,
   },
   
 })
